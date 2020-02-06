a282a52525064c8d1139b4c5417e92756b076df1
William Aristea Tantiono <williamaristeatantiono@william.local>
2020-02-06 17:53:16 +0700

    - Read targets from a file - Write results from a file

--- index.js
+++ index.js
@@ -4,6 +4,27 @@ const url = require('url');
 const fs = require('fs');
 const puppeteer = require('puppeteer');
 
+// Read targets from a file
+async function readTargets(filePath){
+  if (!filePath) return 'https://www.google.com';
+
+  var targets =  fs.readFileSync(filePath).toString().split("\n");
+  targets = await targets.filter(Boolean);
+  return await targets
+}
+
+function writeResults(urls_set, resultPath){
+  resultPath = resultPath ? resultPath : './results.txt';
+
+  fs.writeFileSync(resultPath, "");
+  urls_set.forEach((item) => {
+    fs.appendFileSync(resultPath, item+"\n");
+  });
+
+  console.log("file is written to "+resultPath);
+}
+
+// Get base url
 function formatURL(href, base) {
   try {
     return url.format(new URL(href), { fragment: false });
@@ -12,41 +33,51 @@ function formatURL(href, base) {
   }
 }
 
-async function scrap(){
+// Scrap
+async function scrap(targets){
   const browser = await puppeteer.launch({ dumpio: true });
-  const page = await browser.newPage();
-  await page.exposeFunction('formatURL', formatURL);
 
-  const response = await page.goto('http://test.com');
-  await page.waitFor(5000);
-  const pageUrl = page.url();
+  var urls = []; // not a set just in case number of occurence needs to be counted
 
-  var urls = [];
-  urls = await page.evaluate(async (pageUrl, urls) => {
-    const anchors = Array.from(document.querySelectorAll('a'));
+  for (const target of targets){
+    const page = await browser.newPage(); // create new page
+    await page.exposeFunction('formatURL', formatURL);
+    const response = await page.goto(target); // access target
+    await page.waitFor(5000);
+    const pageUrl = page.url();
 
-    for (const anchor of anchors) {
-      const href = anchor.getAttribute('href');
-      const hrefUrl = await formatURL(href, pageUrl);
-      urls.push(hrefUrl);
-      //console.log(hrefUrl); // isnt used because puppeteer overrides log output
-    }
-    return urls;
-  }, pageUrl, urls);
+    var curr_page_urls = []
 
-  await page.close();
+    curr_page_urls = await page.evaluate(async (pageUrl, curr_page_urls) => {
+      const anchors = Array.from(document.querySelectorAll('a')); // get <a> tag
+
+      for (const anchor of anchors) {
+        const href = anchor.getAttribute('href'); // get href attr
+        const hrefUrl = await formatURL(href, pageUrl); // get base url
+        curr_page_urls.push(hrefUrl);
+      }
+      return curr_page_urls;
+    }, pageUrl, curr_page_urls);
+
+    await page.close();
+
+    urls = urls.concat(curr_page_urls);
+    console.log("["+target+"]"+" is scrapped");
+  }
+
   await browser.close();
   //console.log(urls);
   return urls;
 }
 
-var urls = scrap().then(urls => {
-  const urls_set = new Set(urls);
+// Main
+const args = process.argv.slice(2);
+const targetPath = args[0] ? args[0].toString() : undefined;
+const resultPath = args[1] ? args[1].toString() : undefined;
 
-  console.log(urls_set);
-
-  fs.writeFileSync('/tmp/test.txt', urls_set);
-  console.log("file is written to /tmp/test.txt");
-
-  return urls_set;
+readTargets(targetPath).then((targets,resultPath)=>{
+  console.log(targets);
+  scrap(targets).then(urls => {
+    writeResults(new Set(urls), resultPath)
+  });
 });
