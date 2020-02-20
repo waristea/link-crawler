b296dc17c7ccb0e74e0b2fabd016617f2bd85e81
william.tantiono <william.tantiono@go-jek.com>
2020-02-20 11:18:31 +0700

    Add hard-coded blacklist

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const fs = require('fs');
 const puppeteer = require('puppeteer');
 
 // Read targets from a file
-async function readTargets(filePath){
+async function readTargets(filePath, default){
   if (!filePath) return ['https://www.youtube.com'];
 
   var targets =  fs.readFileSync(filePath).toString().split("\n");
-  targets = await targets.filter(Boolean);
+  targets = await targets.filter(Boolean); // Filter for empty string
   return await targets
 }
 
@@ -27,13 +27,40 @@ function writeResults(urls_set, resultPath, is_printed){
   console.log("file is written to "+resultPath);
 }
 
-// Get base url
+// Get combine with base url if not
 function formatURL(href, base) {
   try {
     return url.format(new URL(href), { fragment: false });
   } catch (e) {
     return url.format(new URL(href, base), { fragment: false });
   }
+}
+
+// Get base url
+function getBase(url){
+  var normalizedUrl = urlNormalize(url);
+
+  return normalizedUrl.split("/").slice(0,3).join("/") // i.e. http://www.google.com/search?q=1337
+}
+
+// Normalize url
+function urlNormalize(url){
+  url = url.split("/");
+  try{
+    if (url.length == 0) return "http://"+url[0] // i.e. www.google.com
+  } catch (e){
+    console.log("Irregular URL Format")
+  }
+  return url.join("/")
+}
+
+// Filter for blacklist (result will still be recorded, but wont be crawled)
+function blacklistFilter(urls, blacklistPath){ // hard match
+  return readTargets(blacklistPath).then(b => {
+    return urls.filter((u) => {
+      return !b.includes(u);
+    });
+  });
 }
 
 // Scrap
@@ -77,10 +104,21 @@ async function scrap(targets){
 const args = process.argv.slice(2);
 const targetPath = args[0] ? args[0].toString() : undefined;
 const resultPath = args[1] ? args[1].toString() : undefined;
+const blacklistPath = args[2] ? args[2].toString() : undefined;
+const depth = args[3] ? args[3].toString() : undefined;
 
 readTargets(targetPath).then((targets,resultPath)=>{
   console.log(targets);
   scrap(targets).then(urls => {
-    writeResults(new Set(urls), resultPath, true)
+    var urlSet = new Set(urls);
+    writeResults(urlSet, resultPath, true);
+
+    var baseUrls = new Set();
+    urlSet.forEach(url => baseUrls.add(getBase(url))); // get base only
+    console.log("Base urls:");
+    console.log(Array.from(baseUrls));
+
+    console.log("Filtered:");
+    blacklistFilter(Array.from(baseUrls), "./blacklist.txt").then(u => console.log(u));
   });
 });
